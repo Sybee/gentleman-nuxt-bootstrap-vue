77f7ff6d27835a85d906f74ab8c21ef17d8908ae
Sybee <imsybee@gmail.com>
2020-11-24 03:13:44 +0300

    init

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-container>
+    <b-btn variant="primary" show>DEFAULT PRIMARY COLOR</b-btn>
+    <b-btn variant="danger" show>DEFAULT DANGER COLOR</b-btn>
+    <b-btn variant="custom">CUSTOM BOOTSTRAP THEME COLOR</b-btn>
+    <b-btn variant="custom">CUSTOM SCSS VAR COLOR</b-btn>
+  </b-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+// Если нужно использовать SCSS-переменные в компонентах по всему проекту, надо добавить модуль
+// npm i @nuxtjs/style-resources -S
+// И закинуть его в nuxt.config.js
+// .custom {
+//   background-color: $custom-color;
+// }
+</style>
